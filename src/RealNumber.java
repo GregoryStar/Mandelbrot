bb674d4cac489356464a7996d3f850218a6d13dc
Gregory Star <starx013@umn.edu>
2018-01-04 14:23:54 -0600

    RealNumber
    Created a RealNumber class which is not currently finished (so it is commented out). This class should help with the precision problem that occurs when zoomed in too much.

---
+++ src/RealNumber.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+/**
+ * Created by F on 1/3/2018.
+ */
+/*
+public class RealNumber {
+    private ArrayList<Integer> rightOfDecimal;
+    private ArrayList<Integer> leftOfDecimal;
+
+    public RealNumber(int i){
+        rightOfDecimal = new ArrayList<Integer>();
+        leftOfDecimal = new ArrayList<Integer>();
+        while(i != 0){
+            rightOfDecimal.add(new Integer(i % 10));
+            i /= 10;
+        }
+    }
+
+    public RealNumber(ArrayList<Integer> rightOfDecimal, ArrayList<Integer> leftOfDecimal){
+        this.rightOfDecimal = rightOfDecimal;
+        this.leftOfDecimal = leftOfDecimal;
+    }
+
+    public RealNumber add(RealNumber lValue){
+        ArrayList<Integer> newRightOfDecimal = new ArrayList<Integer>();
+        ArrayList<Integer> newLeftOfDecimal = new ArrayList<Integer>();
+        long maxLength = rightOfDecimal.size();
+        if(rightOfDecimal.size() < )
+    }
+
+    public long getSize(){
+        return rightOfDecimal.size();
+    }
+
+    public long getPrecision(){
+        return leftOfDecimal.size();
+    }
+}
+*/
